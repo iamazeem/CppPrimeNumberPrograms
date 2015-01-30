f988d518a55c80b70768594708fad9d19af8bd99
Azeem <azeem.sajid@gmail.com>
2015-01-30 13:15:05 +0500

    Prime Numbers Range with Function

---
+++ PrimeNumbersRangeFunction.cpp
@@ -0,0 +1,44 @@
+// Program: C++ Prime Numbers Range with Function
+// GitHub : https://github.com/iamAzeem/CppPrimeNumberPrograms
+
+// Author : AZEEM
+// GitHub : https://github.com/iamAzeem
+// Facebk : https://www.facebook.com/az33msajid
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime( unsigned int number );
+
+int main(void)
+{
+	unsigned int primeRange = 0;
+	cout << "Enter a range for prime numbers: ";
+	cin  >> primeRange;
+
+	cout << endl
+	     << "Prime numbers from 2 to " << primeRange << " are:"
+	     << endl
+	     << " 2 ";	// There's only one even prime number i.e. 2;
+			// we can skip all the remaining even numbers.
+
+	for( unsigned int number = 3; number <= primeRange; number += 2 )
+	{
+		if( isPrime(number) )
+		{
+			cout << number << " ";
+		}
+	}
+	return 0;
+}
+
+bool isPrime( unsigned int number )
+{
+	const double squareRoot = sqrt( number );
+	for( unsigned int i = 2; i <= squareRoot; i++ )
+	{
+		if( number % i == 0 ) return false;
+	}
+	return true;
+}
